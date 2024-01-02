948eb93cd7e79272479f80e4e68d6b55e257b8ce
imadovetch <ibouderoua63@gmail.com>
2024-01-02 01:03:42 +0100

    ssh task

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Puppet to make changes to our configuration file
+
+file_line { 'Turn off passwd auth':
+  ensure => present,
+  line   => '    PasswordAuthentication no',
+  path   => '/etc/ssh/ssh_config',
+}
+
+file_line { 'Declare identity file':
+  ensure => present,
+  line   => '    IdentityFile ~/.ssh/school',
+  path   => '/etc/ssh/ssh_config',
+}
