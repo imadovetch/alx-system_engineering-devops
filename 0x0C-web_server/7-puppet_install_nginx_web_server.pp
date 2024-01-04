a19b5d89f27861955c4b40ab4958a4d57e1e4057
imadovetch <ibouderoua63@gmail.com>
2024-01-05 00:52:52 +0100

    fixes

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
-# Configure server using puppet
+# Configure server
 
-# defines a Puppet class called nginx_server that 
-#  encapsulates the configuration for the Nginx server.
 class nginx_server {
   package { 'nginx':
     ensure => installed,
   }
 
-#  manages the Nginx service.
   service { 'nginx':
     ensure => running,
     enable => true,
     require => Package['nginx'],
   }
-# manages the Nginx configuration file located at /etc/nginx/sites-available/default.
+
   file { '/etc/nginx/sites-available/default':
     ensure  => present,
     content => "
@@ -36,5 +33,5 @@ class nginx_server {
     notify => Service['nginx'],
   }
 }
-#  includes the nginx_server class, ensuring that it gets applied.
+
 include nginx_server
